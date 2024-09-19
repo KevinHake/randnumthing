b60a50183ee68f9fa1f6ca31396aa0a69430b64b
Kevin Hake <kevin@adec.dev>
2024-09-19 15:57:40 -0700

    goofing around with this standalone thing

--- standalone_embed.py
+++ standalone_embed.py
@@ -3,11 +3,13 @@ from bokeh.models import ColumnDataSource, Slider
 from bokeh.plotting import figure
 from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
 from bokeh.server.server import Server
+from time import sleep
 
+df = sea_surface_temperature.copy()
+source = ColumnDataSource(data=df)
 
 def bkapp(doc):
-    df = sea_surface_temperature.copy()
-    source = ColumnDataSource(data=df)
+
 
     plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
                   title="Sea Surface Temperature at 43.18, -70.43")
@@ -33,8 +35,23 @@ def bkapp(doc):
 server = Server({'/': bkapp}, num_procs=1)
 server.start()
 
+
+def cbnew(attr, old, new):
+    print(f"attr: {attr}, old: {old}, new: {new}")
+    if new == 0:
+        data = df
+    else:
+        data = df.rolling(f"{new}D").mean()
+    source.data = ColumnDataSource.from_df(data)
+
 if __name__ == '__main__':
     print('Opening Bokeh application on http://localhost:5006/')
 
     server.io_loop.add_callback(server.show, "/")
+
     server.io_loop.start()
+
+    for i in range(10):
+        print(i)
+        sleep(2)
+        server.io_loop.add_callback(cbnew, 'derp', i, i+1)
