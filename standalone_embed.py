3c88be1e078d0c12e063a14521202e2374c99b9e
Kevin Hake <kevin@adec.dev>
2024-09-19 18:50:14 -0700

    hacky proof of concept for async modifying plot data

--- standalone_embed.py
+++ standalone_embed.py
@@ -4,17 +4,24 @@ from bokeh.plotting import figure
 from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
 from bokeh.server.server import Server
 from time import sleep
+import threading
+
+from bokeh.document import Document
+
 
 df = sea_surface_temperature.copy()
 source = ColumnDataSource(data=df)
-
+d: Document | None = None
+i = 0
 def bkapp(doc):
 
+    global d
+    d = doc
 
     plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
                   title="Sea Surface Temperature at 43.18, -70.43")
     plot.line('time', 'temperature', source=source)
-
+        
     def callback(attr, old, new):
         print(f"attr: {attr}, old: {old}, new: {new}")
         if new == 0:
@@ -23,20 +30,24 @@ def bkapp(doc):
             data = df.rolling(f"{new}D").mean()
         source.data = ColumnDataSource.from_df(data)
 
+    def cb2():
+        print("Periodic callback called")
+        global i
+        callback("Periodic", 0, i)
+        i += 1
     slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
     slider.on_change('value', callback)
-
+    #doc.add_periodic_callback(cb2, 1000)
     doc.add_root(column(slider, plot))
 
 
-# Setting num_procs here means we can't touch the IOLoop before now, we must
-# let Server handle that. If you need to explicitly handle IOLoops then you
-# will need to use the lower level BaseServer class.
-server = Server({'/': bkapp}, num_procs=1)
+server = Server({'/': bkapp})
 server.start()
 
 
 def cbnew(attr, old, new):
+    #session = server.get_sessions('/')[0]
+    #add_next_tick_callback
     print(f"attr: {attr}, old: {old}, new: {new}")
     if new == 0:
         data = df
@@ -44,14 +55,29 @@ def cbnew(attr, old, new):
         data = df.rolling(f"{new}D").mean()
     source.data = ColumnDataSource.from_df(data)
 
+def cbnew2():
+    print("Periodic callback called")
+    global i
+    cbnew("Periodic", 0, i)
+    i += 1
+
+def run_io_loop():
+    server.io_loop.add_callback(server.show, "/")
+    server.io_loop.start()
+
+def my_loop():
+    for i in range(10):
+        sleep(1)
+        print(f"queueing callback for i={i}")
+        global d
+        d.add_next_tick_callback(cbnew2)
+
 if __name__ == '__main__':
     print('Opening Bokeh application on http://localhost:5006/')
 
-    server.io_loop.add_callback(server.show, "/")
+    # Create and start a thread for the io_loop
+    io_loop_thread = threading.Thread(target=run_io_loop)
+    io_loop_thread.start()
 
-    server.io_loop.start()
-
-    for i in range(10):
-        print(i)
-        sleep(2)
-        server.io_loop.add_callback(cbnew, 'derp', i, i+1)
+    # Run the other loop in the main thread
+    my_loop()
